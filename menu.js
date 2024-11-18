e50bdd9cc7672ce61f326c8312c4f99d27a5440f
Saulo Sevalho <127228634+Saulo092@users.noreply.github.com>
2024-11-18 01:06:29 -0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -13,3 +13,17 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+// Função para alternar a visibilidade dos cards e alterar o texto do botão
+function toggleCards() {
+    var cardContainer = document.getElementById("cardContainer");
+    var toggleButton = document.getElementById("toggleButton");
+
+    // Verifica se os cards estão visíveis ou não
+    if (cardContainer.style.display === "none") {
+        cardContainer.style.display = "block";  // Exibe os cards
+        toggleButton.innerText = "1 Trimestre";  // Altera o texto do botão
+    } else {
+        cardContainer.style.display = "none";  // Esconde os cards
+        toggleButton.innerText = "1 Trimestre";  // Altera o texto do botão
+    }
+}
